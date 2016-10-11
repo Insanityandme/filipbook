841d46a57ce62c3c89e3767fd0e3779d04defb8e
insanityandme <bengtegardbook@gmail.com>
2016-10-11 16:27:08 +0200

    logging

--- mysite/config/settings/production.py
+++ mysite/config/settings/production.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 # HTTPS
@@ -8,3 +9,19 @@ SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
