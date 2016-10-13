2b58dd81672aa9f6f7a51da04abb7a7a27d02ed5
insanityandme <bengtegardbook@gmail.com>
2016-10-13 19:33:51 +0200

    storage shit

--- mysite/config/settings/production.py
+++ mysite/config/settings/production.py
@@ -11,20 +11,15 @@ SECURE_SSL_REDIRECT = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 
-# Using AWS S3 to store file uploads
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
-# AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")  # noqa: F405
-# AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")  # noqa: F405
-# AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")  # noqa: F405
-
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)  # noqa: F405
+# Using AWS S3 to store media files
 
 AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")  # noqa: F405
 AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")  # noqa: F405
 AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")  # noqa: F405
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN  # noqa: F405
-MEDIA_ROOT = ''  # noqa: F405
+MEDIA_ROOT = '/media/'  # noqa: F405
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)  # noqa: F405
