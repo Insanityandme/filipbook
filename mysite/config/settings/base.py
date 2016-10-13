60250b8f2b433d712a343733c17232197e9a65f8
insanityandme <bengtegardbook@gmail.com>
2016-10-13 20:03:31 +0200

    lol

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -145,3 +145,12 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")  # noqa: F405
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")  # noqa: F405
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")  # noqa: F405
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN  # noqa: F405
+MEDIA_ROOT = '/media/'  # noqa: F405
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
