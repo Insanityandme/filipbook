01b03408e10dd60da51a5c1d62624e512ae079b2
insanityandme <bengtegardbook@gmail.com>
2016-10-13 21:08:59 +0200

    changes

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -146,11 +146,13 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")  # noqa: F405
-AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")  # noqa: F405
-AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")  # noqa: F405
+# Use AWS S3 bucket to store media files
+
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN  # noqa: F405
-MEDIA_ROOT = '/media/'  # noqa: F405
+MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+MEDIA_ROOT = ''
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
