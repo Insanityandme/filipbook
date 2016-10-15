1e41b90d39beb87e182825c25cfdf31eb0d9ab25
insanityandme <bengtegardbook@gmail.com>
2016-10-15 14:39:00 +0200

    staging config fixed

--- mysite/config/settings/staging.py
+++ mysite/config/settings/staging.py
@@ -5,4 +5,8 @@ DEBUG = True
 
 ALLOWED_HOSTS += ['filipbook-staging.herokuapp.com']  # noqa: F405
 
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+debug_toolbar = 'debug_toolbar.middleware.DebugToolbarMiddleware'
+MIDDLEWARE.insert(0, debug_toolbar)  # noqa: F405
+
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)  # noqa: F405
