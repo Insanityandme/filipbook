4b7125f328100fa56cd4e80d1ef64b453f230193
insanityandme <bengtegardbook@gmail.com>
2016-10-15 13:57:33 +0200

    database config issues

--- mysite/config/settings/staging.py
+++ mysite/config/settings/staging.py
@@ -1,5 +1,8 @@
+import dj_database_url
 from .base import *  # noqa: F403
 
 DEBUG = True
 
 ALLOWED_HOSTS += ['filipbook-staging.herokuapp.com']  # noqa: F405
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)  # noqa: F405
